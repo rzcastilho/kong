049a1b9784e2d3b06b3ec705b7096daf1ebca708
Rodrigo Zampieri Castilho <rodrigo.zampieri@gmail.com>
2017-06-21 17:45:34 -0300

    Update kong start script to wait for kong-database

--- run/kong.sh
+++ run/kong.sh
@@ -19,6 +19,11 @@ if [ -e /var/run/$DAEMON.pid ]; then
   kill $PID
 fi
 
+while ! nc -q 1 $KONG_PG_HOST 5432 </dev/null; do
+	echo "Waiting $KONG_PG_HOST..."
+	sleep 5
+done
+
 # Start process and save PID
 nohup $DAEMON $ARGS 1>/var/log/$DAEMON.out 2>/var/log/$DAEMON.err &
 echo $! > /var/run/$DAEMON.pid
